a5cd40a12b713eb3e0a21f36d89ea37776a10003
kiyokun08 <168139494+kiyokun08@users.noreply.github.com>
2024-07-23 15:30:35 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -16,22 +16,22 @@ global.mtl_language = false // multi language i hman dawn chuan true ah dah tur
 global.bot_language = 'lus' // I language code dah tur code hman i hriat loh chuan https://herbert70.blogspot.com/p/language-code-for-hbwabot.html?m=1 he link ah hian en thei a ni ang
 
 //info
-global.owner = ['918787723629'] // I phone number in thlak rawh
-global.ownernumber = '918787723629' // I phone number in thlak rawh
-global.ownername = "ruatsanga" // I hming dah tel bawk la
+global.owner = ['917085628554'] // I phone number in thlak rawh
+global.ownernumber = '917085628554' // I phone number in thlak rawh
+global.ownername = "zualteii" // I hming dah tel bawk la
 global.location = "India, Mizoram, Aizawl" // I awmna hmun daha
-global.botname = "ruatsanga" // I bot hming tur dah la
+global.botname = "zualteii" // I bot hming tur dah la
 global.ownerweb = "https://youtube.com/@HBMods_Channel"
 global.themeemoji = '🤖'
-global.creator = "918787723629@s.whatsapp.net" // @ symbol hmaa number hi i phone number in thlak rawh
+global.creator = "917085628554@s.whatsapp.net" // @ symbol hmaa number hi i phone number in thlak rawh
 
-global.ownerNumber = ["918787723629@s.whatsapp.net"]// @ symbol hmaa number hi i phone number in thlak rawh
+global.ownerNumber = ["917085628554@s.whatsapp.net"]// @ symbol hmaa number hi i phone number in thlak rawh
 global.prefa = ['','!','.','#','/']
 global.sessionName = 'session'
 
 // Sticker Pack Name
-global.packname = "Sticker By ruatsanga"
-global.author = "ruatsanga"
+global.packname = "Sticker By zualteii"
+global.author = "zualteii"
 global.hbmods = "https://herbert70.blogspot.com/"
 //_______________________________________________
 global.defaultpp = 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png?q=60'
